44138e40ed58102d65576590a3e7960ea5dc2143
Jonathan East <jonathan_east@ymail.com>
2021-01-13 11:19:30 -0500

    added create, update, and destroy methods in Bands Controller; tested code

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -10,5 +10,34 @@ class BandsController < ApplicationController
         band = Band.find(params[:id])
         render json: band
     end
+
+    def create
+        band = Band.new(band_params)
+
+        if band.save
+            render json: band
+        end
+    end
+
+    def update
+        band = Band.find(params[:id])
+
+        if band.update(band_params)
+            render json: band
+        end
+
+    end
+
+    def delete
+        band = Band.find(params[:id])
+        band.destroy
+    end
+
+    private
+
+    def band_params() 
+        params.require(:band).permit(:band_name)
+    end
+
     
 end
