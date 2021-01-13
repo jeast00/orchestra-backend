84ca9531205abd20146ca799a1beb2d83d52832f
Jonathan East <jonathan_east@ymail.com>
2021-01-13 10:45:14 -0500

    added band_id to CreateInstruments table

--- db/migrate/20210113154133_create_instruments.rb
+++ db/migrate/20210113154133_create_instruments.rb
@@ -2,7 +2,7 @@ class CreateInstruments < ActiveRecord::Migration[6.0]
   def change
     create_table :instruments do |t|
       t.string :instrument_name
-
+      t.integer :band_id
       t.timestamps
     end
   end
